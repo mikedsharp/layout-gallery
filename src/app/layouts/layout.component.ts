9b32a02cc997d6cf2aeecec3b45019c75af95c9e
Mike Sharp <msharp23@hotmail.co.uk>
2016-08-01 22:31:13 +0100

    first design, very simple, uses flexbox

---
+++ src/app/layouts/layout.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'layout1',
+    templateUrl: './layout1.template.html',
+    styles: [require('./layout1.scss')]
+})
+
+export class LayoutComponent {
+
+}
--- src/app/layouts/layouts.routes.ts
+++ src/app/layouts/layouts.routes.ts
@@ -1,7 +1,9 @@
 import {RouterConfig} from '@angular/router';
 import {LayoutListComponent} from "./layoutList.component";
+import {LayoutComponent} from "./layout.component";
 
 export const layoutsRoutes: RouterConfig = [
     {path: '', component: LayoutListComponent},
-    {path: 'layouts', component: LayoutListComponent}
+    {path: 'layouts', component: LayoutListComponent},
+    {path: 'layout/:id', component: LayoutComponent}
 ];
