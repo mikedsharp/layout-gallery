663d844238630c58260702b9bf381b165fe1b6b9
Mike Sharp <msharp23@hotmail.co.uk>
2016-08-01 22:50:39 +0100

    changed to using HashStrategy, because I cannot get routing working in production without it!

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { AppComponent } from './app/app.component';
 import  {APP_ROUTER_PROVIDERS} from './app/app.routes';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
+
 
 
 if (process.env.ENV === 'production') {
     enableProdMode();
 }
-bootstrap(AppComponent, [APP_ROUTER_PROVIDERS]);
+bootstrap(AppComponent, [APP_ROUTER_PROVIDERS,  { provide: LocationStrategy, useClass: HashLocationStrategy }]);
