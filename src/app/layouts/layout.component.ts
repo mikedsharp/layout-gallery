2f3fa567d98dad2b85325e6bd9be8dc28f410929
programmerinprogress <msharp23@hotmail.co.uk>
2016-08-03 00:02:25 +0100

    Started on a funky looking full-screen image-based template
    I would really like to use one of these in my final portfolio, so it's good to know how they work

--- src/app/layouts/layout.component.ts
+++ src/app/layouts/layout.component.ts
@@ -1,9 +1,9 @@
 import {Component} from '@angular/core';
 
 @Component({
-    selector: 'layout1',
-    templateUrl: './layout1.template.html',
-    styles: [require('./layout1.scss')]
+    selector: 'layout2',
+    templateUrl: './layout2.template.html',
+    styles: [require('./layout2.scss')]
 })
 
 export class LayoutComponent {
